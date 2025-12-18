9183b4140a604f4da53da7ec743179b3d7eb3df1
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2025-12-18 19:42:21 +0100

    feat: robot turn 90deg and correct itsefl if needed

--- controllers/turtlebot_wall-following/turtlebot_wall-following.py
+++ controllers/turtlebot_wall-following/turtlebot_wall-following.py
@@ -1,181 +1,156 @@
 """turtlebot_wall-following controller"""
 
-# You can us this template to implement the behaviors and maze sover state-machine
-# for the Turtlebot3.
-
-import numpy as np
-from controller import Robot  # , DistanceSensor, Motor, Compass
-
-# -------------------------------------------------------
-# Initialize variables
-
-MAX_SPEED = 40  # 6.28
-
-# create the Robot instance.
-robot = Robot()
-
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())  # [ms]
-delta_t = robot.getBasicTimeStep() / 1000.0  # [s]
-
-# states
-states = ["forward", "turn", "backward"]
-current_state = states[0]
-
-# counter: used to maintain an active state for a number of cycles
-counter = 0
-COUNTER_MAX = 181
-
-# Robot wheel speeds
-wl = 0.0  # angular speed of the left wheel [rad/s]
-wr = 0.0  # angular speed of the right wheel [rad/s]
-
-# Robot linear and angular speeds
-u = 0.0  # linear speed [m/s]
-w = 0.0  # angular speed [rad/s]
-
-# Physical parameters for the kinematics model (constants)
-R = 0.330  # radius of the wheels [m]
-D = 0.160  # distance between the wheels [m]
-
-# Controller gains - define how the reaction of the robot will be:
-# higher controller gain will result in faster reaction, but it can cause oscillations
-k_1 = 1
-k_2 = 1
-
-# -------------------------------------------------------
-# Initialize devices
-
-# 2D-LiDAR
-lidar = robot.getDevice("LDS-01")
-lidar.enable(timestep)
-lidar.enablePointCloud()
-pointCloud = []  # list to store the point cloud data
-
-# encoders
-encoder = []
-encoderNames = ["left wheel sensor", "right wheel sensor"]  # defined in the robot model
-for i in range(2):
-    encoder.append(robot.getDevice(encoderNames[i]))
-    encoder[i].enable(timestep)
-
-oldEncoderValues = []
-
-# motors
-leftMotor = robot.getDevice("left wheel motor")  # name defined in the robot model
-rightMotor = robot.getDevice("right wheel motor")  # name defined in the robot model
-leftMotor.setPosition(float("inf"))
-rightMotor.setPosition(float("inf"))
-leftMotor.setVelocity(0.0)
-rightMotor.setVelocity(0.0)
+from controller import Robot
 
 
-#######################################################################
-# Functions
-# write here any function you may need: wheel speed computation, robot speed computation,
-# wall-following behavior, etc.
+class TurtlebotWallFollowing:
+    # --- Constants ---
+    # Lidar Angles (Indices for the point cloud array)
+    L_LEFT = 90
+    L_FRONT = 180
+    L_RIGHT = 270
+    L_BACK = 0
 
+    # Robot Physical Parameters
+    R = 0.330  # radius of the wheels [m]
+    D = 0.160  # distance between the wheels [m]
 
-def wheel_speed_commands(u_d, w_d, D, R):
-    """Converts desired speeds to wheel speed commands
-    Inputs:
-        u_d = desired linear speed for the robot [m/s]
-        w_d = desired angular speed for the robot [rad/s]
-        R = radius of the robot wheel [m]
-        D = distance between the left and right wheels [m]
-    Returns:
-        wr_d = desired speed for the right wheel [rad/s]
-        wl_d = desired speed for the left wheel [rad/s]
-    """
-    wr_d = float((2 * u_d + D * w_d) / (2 * R))
-    wl_d = float((2 * u_d - D * w_d) / (2 * R))
+    # State Machine Thresholds
+    COUNTER_MAX = 180
+    OBSTACLE_THRESHOLD = 0.2
+    WALL_FOLLOW_DIST = 0.3
+    ALIGNMENT_THRESHOLD = 0.05
 
-    return wl_d, wr_d
+    # States
+    STATE_IDLE = "idle"
+    STATE_FORWARD = "forward"
+    STATE_LEFT = "left"
+    STATE_RIGHT = "right"
+    STATE_BACKWARD = "backward"
 
+    def __init__(self):
+        self.robot = Robot()
+        self.timestep = int(self.robot.getBasicTimeStep())
 
-#######################################################################
-# Main loop: See-think-act cycle
-# Perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # ----------------------------- See -----------------------------------
-    # Read the LiDAR data
-    pointCloud = lidar.getRangeImage()  # [m], 360 values, one per degree
+        # State Initialization
+        self.current_state = self.STATE_IDLE
+        self.current_action = ""
+        self.counter = 0
 
-    encoderValues = []
-    for i in range(2):
-        encoderValues.append(encoder[i].getValue())  # [rad]
+        # Hardware Initialization
+        self._init_devices()
 
-    # Update old encoder values if not done before
-    if len(oldEncoderValues) < 2:
-        for i in range(2):
-            oldEncoderValues.append(encoder[i].getValue())
+    def _init_devices(self):
+        """Initialize robot devices: Lidar and Motors."""
+        # 2D-LiDAR
+        self.lidar = self.robot.getDevice("LDS-01")
+        self.lidar.enable(self.timestep)
+        self.lidar.enablePointCloud()
 
-    # ----------------------------- Think ---------------------------------
-    # Implement the finite-state machine to select the robot behavior
-    # Example:
-    if current_state == "forward":
-        u_d = 2.0
-        w_d = 0.0
+        # Motors
+        self.left_motor = self.robot.getDevice("left wheel motor")
+        self.right_motor = self.robot.getDevice("right wheel motor")
+        self.left_motor.setPosition(float("inf"))
+        self.right_motor.setPosition(float("inf"))
+        self.left_motor.setVelocity(0.0)
+        self.right_motor.setVelocity(0.0)
 
-    if current_state == "left":
-        u_d = 0.0
-        w_d = 6.0
+    def _convert_velocity_to_wheel_speed(self, u_d, w_d):
+        """
+        Converts desired linear (u_d) and angular (w_d) speeds to wheel speeds.
+        Returns: (left_wheel_speed, right_wheel_speed)
+        """
+        wr_d = (2 * u_d + self.D * w_d) / (2 * self.R)
+        wl_d = (2 * u_d - self.D * w_d) / (2 * self.R)
+        return float(wl_d), float(wr_d)
 
-    if current_state == "right":
-        u_d = 0.0
-        w_d = -6.0
+    def _is_obstacle_near(self, point_cloud, angle: int, threshold: float) -> bool:
+        """Check if an obstacle is within threshold at given angle."""
+        return point_cloud[angle] <= threshold
 
-    if current_state == "backward":
-        u_d = -2.0
-        w_d = 0.0
+    def _get_alignment_offset(self, point_cloud, angle: int) -> float:
+        """
+        Calculate offset between left and right rays of a given angle
+        to determine perpendicular alignment.
+        """
+        left_laser = point_cloud[(angle - 1) % 360]
+        right_laser = point_cloud[(angle + 1) % 360]
+        return left_laser - right_laser
 
-    # Transitions:
-    if current_state == "forward":
-        if pointCloud[180] < 0.2:
-            if pointCloud[90] < 0.3:
-                current_state = "right"
+    def _update_state(self, point_cloud):
+        """
+        Determines the next state based on sensor data and current state.
+        Implements the finite state machine logic.
+        """
+        # 1. Do nothing if there is a counter
+        if self.counter > 0:
+            self.counter -= 1
+            return
+
+        # 2. Handle '90turn' Action Sequence
+        if self.current_action == "90turn":
+            offset = self._get_alignment_offset(point_cloud, self.L_BACK)
+            if offset > self.ALIGNMENT_THRESHOLD:
+                self.current_state = self.STATE_LEFT
+            elif offset < -self.ALIGNMENT_THRESHOLD:
+                self.current_state = self.STATE_RIGHT
             else:
-                current_state = "left"
-            counter = 91 # higher initial value for a shorter curve
-        # else:
-        #     if pointCloud[90] < 0.15:
-        #         current_state = 'left'
-        #     if pointCloud[270] < 0.15:
-        #         current_state = 'right'
-        #     counter = 95
+                self.current_action = ""
+                self.current_state = self.STATE_FORWARD
+            return
 
-    # Correction de direction ?
-    # if current_state == "forward":
-    #     print(
-    #         f"distances = {pointCloud[180]:.2f}, fright = {pointCloud[179]:.2f}, fleft = {pointCloud[181]:.2f}"
-    #     )
-    #     offset = pointCloud[181] - pointCloud[179]
-    #     print(offset)
-    #     if offset > 0.1:
-    #         current_state = "left"
-    #         counter = 90  # higher initial value for a shorter curve
-    #     if offset < -0.1:
-    #         current_state = "right"
-    #         counter = 90  # higher initial value for a shorter curve
+        # 3. Default direction logic when hitting a front wall
+        if self._is_obstacle_near(point_cloud, self.L_FRONT, self.OBSTACLE_THRESHOLD):
+            # Decide which way to turn based on left wall distance
+            if point_cloud[self.L_LEFT] < self.WALL_FOLLOW_DIST:
+                self.current_state = self.STATE_RIGHT
+            else:
+                self.current_state = self.STATE_LEFT
 
-    if current_state == "left" or current_state == "right":
-        if counter > COUNTER_MAX:
-            current_state = "forward"
-    # increment counter
-    counter += 1
+            # Initiate turn sequence
+            self.counter = 0.5 * self.COUNTER_MAX
+            self.current_action = "90turn"
+        else:
+            self.current_state = self.STATE_FORWARD
 
-    # update old encoder values for the next cycle
-    oldEncoderValues = encoderValues
+    def _get_target_velocities(self):
+        """Map current state to desired linear (u) and angular (w) velocities."""
+        if self.current_state == self.STATE_FORWARD:
+            return 2.0, 0.0
+        elif self.current_state == self.STATE_LEFT:
+            return 0.0, 6.0
+        elif self.current_state == self.STATE_RIGHT:
+            return 0.0, -6.0
+        elif self.current_state == self.STATE_BACKWARD:
+            return -2.0, 0.0
 
-    # ----------------------------- Act -----------------------------------
-    # Set motor speeds with the values defined by the state-machine
-    leftSpeed, rightSpeed = wheel_speed_commands(u_d, w_d, D, R)
-    leftMotor.setVelocity(leftSpeed)
-    rightMotor.setVelocity(rightSpeed)
+        # Idle or unknown
+        return 0.0, 0.0
 
-    # Debug
-    print(
-        f"Current state = {current_state}, distances = {pointCloud[180]:.2f}, fright = {pointCloud[270]:.2f}, fleft = {pointCloud[90]:.2f}, u_d = {u_d:.2f}, w_d = {w_d:.2f}"
-    )
+    def run(self):
+        """Main control loop."""
+        while self.robot.step(self.timestep) != -1:
+            # --- See ---
+            point_cloud = self.lidar.getRangeImage()
 
-    # Repeat all steps while the simulation is running.
+            # --- Think ---
+            self._update_state(point_cloud)
+            u_d, w_d = self._get_target_velocities()
+
+            # --- Act ---
+            left_speed, right_speed = self._convert_velocity_to_wheel_speed(u_d, w_d)
+            self.left_motor.setVelocity(left_speed)
+            self.right_motor.setVelocity(right_speed)
+
+            # Debug output
+            print(
+                f"State: {self.current_state:8s} | "
+                f"Front: {point_cloud[self.L_FRONT]:.2f} | "
+                f"Left: {point_cloud[self.L_LEFT]:.2f} | "
+                f"Action: {self.current_action}"
+            )
+
+
+if __name__ == "__main__":
+    controller = TurtlebotWallFollowing()
+    controller.run()
